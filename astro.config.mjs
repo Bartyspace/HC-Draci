cd93e14744acfe5466328c1a4f3c955374804980
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-06-02 17:27:25 +0200

    i

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
-
 import sitemap from "@astrojs/sitemap";
+import SpeedInsights from "@vercel/speed-insights/astro";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), playformCompress(), sitemap()]
+  integrations: [tailwind(), playformCompress(), sitemap(), SpeedInsights()],
 });
